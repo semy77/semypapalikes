53d2781e960dd72b10f32615ca36be283bf7360e
semy77 <semypapa7724@gmail.com>
2026-05-25 00:01:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -128,7 +128,7 @@ def make_request(encrypt, server_name, token):
         elif server_name in {"BR", "US", "SAC", "NA"}:
             url = "https://client.us.freefiremobile.com/GetPlayerPersonalShow"
         else:
-            url = "https://clientbp.ggblueshark.com/GetPlayerPersonalShow"
+            url = "https://clientbp.ggpolarbear.com/GetPlayerPersonalShow"
         edata = bytes.fromhex(encrypt)
         headers = {
             'User-Agent': "Dalvik/2.1.0 (Linux; U; Android 9; ASUS_Z01QD Build/PI)",
@@ -244,7 +244,7 @@ def handle_requests():
             elif server_name_used in {"BR", "US", "SAC", "NA"}:
                 url = "https://client.us.freefiremobile.com/LikeProfile"
             else:
-                url = "https://clientbp.ggblueshark.com/LikeProfile"
+                url = "https://clientbp.ggpolarbear.com/LikeProfile"
 
             asyncio.run(send_multiple_requests(uid, server_name_used, url))
 
